e7b6fd6fa7a6744dc0b52735c2f17f0fc3ac394b
yekhanfir <mail.yessinekhanfir@gmail.com>
2025-03-05 15:33:13 +0100

    add helper functions to data uitls

--- src/data_handling/data_utils.py
+++ src/data_handling/data_utils.py
@@ -1,5 +1,6 @@
 from src import (
-   ATOM_TYPES, 
+   ATOM_TYPES,
+   RESTYPES,
    RESTYPE_1TO3, 
    RESTYPE_NUM, 
    PDB_MAX_CHAINS, 
@@ -8,6 +9,7 @@ from src import (
 )
 import numpy as np
 import pandas as pd
+import torch
 
 
 def make_np_example(coords_dict):
@@ -69,6 +71,52 @@ def _chain_end(atom_index, end_resname, chain_name, residue_index) -> str:
           f'{chain_name:>1}{residue_index:>4}')
 
 
+def get_np_structure(sample):
+        """
+        Return the np_structure to build an instance of the Protein class.
+        This was already implemented in the initial notebook, but decided
+        to make it part of the DDPM class for convenience.
+        """
+        # to make this function compatibile with tensor
+        # and numpy inputs for general use
+        if isinstance(sample, torch.Tensor):
+          sample = sample.detach().cpu().numpy()
+        bb_atom_types = ['N', 'CA', 'C', 'O']
+        bb_idx = [i for i, atom_type in enumerate(ATOM_TYPES)
+                  if atom_type in bb_atom_types]
+
+        num_res = len(sample)
+        nan_pos = np.isnan(sample)[..., 0]
+        sample[nan_pos] = 0.
+        atom_mask = np.zeros([num_res, 37])
+        atom_mask[..., bb_idx] = 1
+
+        np_sample = {
+            'atom_positions': sample,
+            'residue_index':np.arange(num_res),
+            'atom_mask': atom_mask,
+        }
+        return np_sample
+
+
+def get_protein(x, max_seq_length):
+        """
+        Returns a Protein instance from a given input.
+        Expects Tensor or Numpy ndarray.
+        first generates the np_structure, then returns a Protein instance.
+        """
+        np_sample = get_np_structure(x)
+        prot = Protein(
+            atom_positions=np_sample['atom_positions'],
+            atom_mask=np_sample['atom_mask'],
+            residue_index=np_sample['residue_index'],
+            aatype=np.zeros([max_seq_length,], dtype=np.int32),
+            chain_index=np.zeros([max_seq_length,], dtype=np.int32),
+            b_factors=np.ones([max_seq_length, 37], dtype=np.int32)
+        )
+        return prot
+
+
 def to_pdb(prot: Protein) -> str:
   """Converts a `Protein` instance to a PDB string.
 
@@ -78,11 +126,7 @@ def to_pdb(prot: Protein) -> str:
   Returns:
     PDB string.
   """
-  restypes = [
-    'A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P',
-    'S', 'T', 'W', 'Y', 'V', 'X'
-  ]
-  res_1to3 = lambda r: RESTYPE_1TO3.get(restypes[r], 'UNK')
+  res_1to3 = lambda r: RESTYPE_1TO3.get(RESTYPES[r], 'UNK')
 
   pdb_lines = []
 
